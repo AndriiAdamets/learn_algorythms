fb4a5b4eef7f34632be64a601dcebeffdbd4faf3
Andrii <andrey.adamets@gmail.com>
2021-07-04 17:48:25 +0300

    add bubblu and quick sorts

--- sort.py
+++ sort.py
@@ -1,3 +1,17 @@
+import random
+
+
+def bubble_sort(data):
+    max_item_index = len(data) - 1
+    for i in range(max_item_index):
+        for j in range(max_item_index - i):
+            if data[j] > data[j + 1]:
+                temp = data[j + 1]
+                data[j + 1] = data[j]
+                data[j] = temp
+    return data
+
+
 def smallest_item_index(data):
     smallest_index = 0
     smallest_item = data[smallest_index]
@@ -16,3 +30,19 @@ def selection_sort(data):
         result.append(data[index])
         del data[index]
     return result
+
+
+def quick_sort(data):
+    if len(data) < 2:
+        return data
+    pivot_index = random.randint(0, len(data) - 1)
+    left = []
+    right = []
+    pivot = data[pivot_index]
+    del data[pivot_index]
+    for i in range(len(data)):
+        if data[i] > pivot:
+            right.append(data[i])
+        else:
+            left.append(data[i])
+    return quick_sort(left) + [pivot] + quick_sort(right)
--- test_sort.py
+++ test_sort.py
@@ -3,8 +3,17 @@ import sort
 
 
 class TestSort(unittest.TestCase):
+    def test_bubble_sort(self):
+        self.assertEqual(sort.bubble_sort([7, 10, 4, 6, 8, 1, 3, 2, 9, 5]), [
+                         1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
     def test_smallest_item_index(self):
         self.assertEqual(sort.smallest_item_index([4, 7, -1, 8, 22]), 2)
 
     def test_selection_sort(self):
-        self.assertEqual(sort.selection_sort([2, 4, 1, 5, 3]), [1, 2, 3, 4, 5])
+        self.assertEqual(sort.selection_sort([7, 10, 4, 6, 8, 1, 3, 2, 9, 5]), [
+                         1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    def test_quick_sort(self):
+        self.assertEqual(sort.quick_sort([7, 10, 4, 6, 8, 1, 3, 2, 9, 5]), [
+                         1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
